7538bcf09b24bce2aee599fe4b7a246dccc61151
phatphampp <phatpham2511@gmail.com>
2023-01-06 22:01:36 +0700

    Update giao dien giao dich

--- client/src/pages/admin/transaction.page.js
+++ client/src/pages/admin/transaction.page.js
@@ -1,45 +1,105 @@
 import { useEffect, useState, useCallback, useMemo } from 'react';
 import { Button, Checkbox, message, Modal, Skeleton, Table } from 'antd';
-import { ManagementList } from '../../components/management-list.component.js';
-import { BankTransactionHistoryForm } from '../../components/bank-transaction-history-form.component.js';
+import styled from '@xstyled/styled-components';
+import Title from 'antd/es/typography/Title.js';
+import { CreateEmployeeForm } from '../../components/create-employee-form.component.js';
 import ContentLayout from '../../components/common/content-layout.component.js';
 
+const FlexLayout = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
 export const TransactionPage = () => {
-    const [
-        isBankTransactionHistoryModalVisible,
-        setBankTransactionHistoryModalVisibility,
-    ] = useState(false);
+    const [listEmployee, setListEmployee] = useState(null);
 
-    const toggleBankTransactionHistoryModalVisibility = () => {
-        setBankTransactionHistoryModalVisibility(
-            !isBankTransactionHistoryModalVisible
-        );
-    };
-
-    const settingItems = useMemo(
+    const historyColumns = useMemo(
         () => [
             {
-                title: 'Xem danh sách giao dịch trong tháng với các ngân hàng khác',
-                description:
-                    'Xem được các giao dịch: Xem trong khoảng thời gian. Xem theo từng ngân hàng, hoặc tất cả ngân hàng liên kết. Có thống kê tổng số tiền đã giao dịch',
-                actionTitle: 'Xem lịch sử',
-                onItemClick: toggleBankTransactionHistoryModalVisibility,
+                title: 'Ngân hàng gửi',
+                dataIndex: 'email',
+                key: 'email',
+            },
+            {
+                title: 'Ngân hàng nhận',
+                dataIndex: 'firstName',
+                key: 'firstName',
+            },
+            {
+                title: 'Tổng số tiền',
+                dataIndex: 'lastName',
+                key: 'lastName',
+            },
+            {
+                title: 'Thời điểm giao dịch',
+                dataIndex: 'phoneNumber',
+                key: 'phoneNumber',
             },
         ],
         []
     );
 
+    useEffect(() => {
+        getListEmployee();
+    }, []);
+
+    const getListEmployee = useCallback(() => {
+        const url = `${process.env.REACT_APP_USER_API_URL_PATH}?role=employee`;
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: localStorage.getItem('accessToken'),
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => setListEmployee(data));
+    }, []);
+
+    const renderListEmployee = () => {
+        if (!listEmployee) {
+            return <Skeleton />;
+        } else {
+            // Danh sách nhân viên
+            // const receiveBillings = paymentAccountHistory.filter(
+            //     (p) => p.type === 'receive'
+            // );
+            const mappedListEmployee = listEmployee.map((r, index) => {
+                return {
+                    key: index,
+                    email: r.email,
+                    firstName: r.firstName,
+                    lastName: r.lastName,
+                    phoneNumber: r.phoneNumber,
+                };
+            });
+
+            return (
+                <>
+                    <p>
+                        <strong>
+                            <i>Danh sách giao dịch</i>
+                        </strong>
+                    </p>
+                    <Table
+                        columns={historyColumns}
+                        dataSource={mappedListEmployee}
+                    />
+                </>
+            );
+        }
+    };
+
+    const ListEmployee = styled.div``;
+
     return (
         <ContentLayout>
-            <Modal
-                title='Theo dõi lịch sử giao dịch các ngân hàng'
-                centered
-                open={isBankTransactionHistoryModalVisible}
-                onOk={toggleBankTransactionHistoryModalVisibility}
-                onCancel={toggleBankTransactionHistoryModalVisibility}>
-                <BankTransactionHistoryForm />
-            </Modal>
-            <ManagementList sources={settingItems} />
+            <ListEmployee>
+                <FlexLayout>
+                    <Title level={2}>Danh sách giao dịch các ngân hàng</Title>
+                </FlexLayout>
+                {renderListEmployee()}
+            </ListEmployee>
         </ContentLayout>
     );
 };
